06ca9f0bf6712438d4daec6c151cffd7eb1f08eb
Paul <p@x128.ru>
2023-02-17 21:14:54 +0300

    not working update

--- src/content/app.js
+++ src/content/app.js
@@ -1,6 +1,36 @@
-console.log('App test')
+// const testEl = document.createElement('div')
+// testEl.classList.add('test')
+// testEl.textContent = 'TEST'
+// document.body.appendChild(testEl)
 
-const testEl = document.createElement('div')
-testEl.classList.add('test')
-testEl.textContent = 'TEST'
-document.body.appendChild(testEl)
+const url = ''
+
+function sendRequst(text) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            user: user_id, 
+            text: text,
+            page: document.getElementsByClassName('app-article_title_1wM7c')[0].textContent
+        }
+    }).then(res => {
+        console.log('Result:', res)
+    })
+    .catch(err => {
+        console.error('Request error:', err)
+    })
+}
+
+function getID() {
+    return fetch('https://grants.myrosmol.ru/api/profile/personal')
+    .then(res => res.json())
+    .then(res => res.ID)
+}
+
+const domContainer = document.createElement('div')
+domContainer.classList.add('root')
+const root = ReactDOM.createRoot(domContainer)
+root.render()
